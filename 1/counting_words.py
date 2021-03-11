6eb4eb2ed0d2a0198f39f4b38fac567a07bcd790
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2021-03-11 15:02:32 +0100

    Fixed script Added lorem ipsum Added sorting

--- 1/counting_words.py
+++ 1/counting_words.py
@@ -1,7 +1,7 @@
 # how many times each word appears in the txt file
 
-fname = input('Enter the file name: ')  #e.g. www.py4inf.com/code/romeo.txt
-
+# fname = input('Enter the file name: ')  #e.g. www.py4inf.com/code/romeo.txt
+fname = "Lorem Ipsum"
 try:
 	fhand = open(fname)
 except:
@@ -13,9 +13,9 @@ for line in fhand:
 	words = line.split()
 
 	for word in words:
-		if word not in counts:
-			counts[word] = 1
+		if word in counts.keys():
+			counts[word] += 1
 		else:
 			counts[word] = 1
 
-print(counts)
+print(sorted(counts.items(), key=lambda k : k[1]))
