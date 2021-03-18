4ddea610318d768b60bb06614099b9d12d0d964b
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2021-03-18 10:09:04 +0100

    Completed Z4

--- 2/Z4.py
+++ 2/Z4.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
 img = mpimg.imread('tiger.png')
 
-print(type(img))
 for i in range(len(img)):
-	for j in range(len(i)):
-		print(img[i][j])
+	for j in range(len(img[i])):
+		img[i, j] = [img[i, j, 0] + 0.2, img[i, j, 1] + 0.2, img[i, j, 2] + 0.2]
+		for k in range(img[i, j]):
+			if img[i, j, k] > 1.0:
+				img[i, j, k] = 1.0
 
 imgplot = plt.imshow(img)
 plt.show()
